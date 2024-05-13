8f8393197fa014a889282f3bce9bb3badf9d7684
Kouta623 <k023g1021@g.neec.ac.jp>
2024-05-13 16:27:30 +0900

    Affine変換編集中

--- DirectXGame/mt3.h
+++ DirectXGame/mt3.h
@@ -483,5 +483,8 @@ Matrix4x4 MakeAffineMatrix(const Vector3& translate, const Vector3& rotate, cons
 
 	Matrix4x4 makeRotate = Multiply(makeRotateXMatrix, Multiply(makeRotateYMatrix, makeRotateZMatrix));
 
-	return Matrix4x4(Multiply(MakeScaleMatrix(scale), Multiply(makeRotate, MakeTranslateMatrix(translate))));
+	Matrix4x4 matScale = MakeScaleMatrix(scale);
+	Matrix4x4 matTranslate = MakeTranslateMatrix(translate);
+
+	return Matrix4x4(Multiply(matScale, Multiply(makeRotate, matTranslate)));
 }
