364a7b1f75485533a444acd8f55b38ca603faa3b
Kouta623 <k023g1021@g.neec.ac.jp>
2024-05-16 13:10:48 +0900

    天球描画途中

---
+++ DirectXGame/Skydome.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<GameScene.h>
+#include<WorldTransform.h>
+class Skydome {
+	public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	private:
+
+		//ワールド変換データ
+		WorldTransform worldTransform_;
+		//モデル
+	    Model* model_ = nullptr;
+
+};
